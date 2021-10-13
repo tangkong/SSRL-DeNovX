080fdea45e50140be9859c303432cb97d408819d
none <natej@stanford.edu>
2021-10-12 18:01:06 -0700

    Helper function for finding sample boundaries relative to a box beam.

---
+++ profile_bluesky/startup/instrument/plans/inscribe.py
@@ -0,0 +1,64 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# circle diameter and center coordinates
+d = 10
+C = np.array([d/2,d/2])
+
+#box width 
+w = 3
+h = 2
+
+# inscribe a box
+n = np.int(np.ceil(d/w))
+m = np.int(np.ceil(d/h))
+#check if the above are even; if not, add 1
+if n % 2 != 0:
+    n = n+1
+if m % 2 != 0:
+    m = m+1
+
+
+# generate an array for the inscribing box
+# calculate start and end points for the box
+xst = C[0] - (w*n)/2
+xen = C[0] + (w*n)/2
+yst = C[1] - (h*n)/2
+yen = C[1] + (h*n)/2
+
+xarr = np.linspace(xst,xen,num=n)
+yarr = np.linspace(yst,yen,num=m)
+
+# generate a grid
+xx, yy = np.meshgrid(xarr,yarr)
+
+# list to store all teh acceptable positions
+xacc = []
+yacc = []
+# okay now step through points on the grid and see if the corners of the box
+# they define fall outside the radius of the circle
+for ii in xarr:
+    for jj in yarr:
+        bnds = [C - [ii-w/2,jj],C -[ii+w/2,jj],
+                C - [ii,jj-h/2],C - [ii,jj+h/2]]
+        test = [0,0,0,0]
+        for ind,bnd in enumerate(bnds):
+            print(np.linalg.norm(bnd))
+            if np.linalg.norm(bnd) > d/2:
+                test[ind] = 1
+
+        if 1 in test:
+            continue
+        else:
+            xacc.append(ii)
+            yacc.append(jj)
+
+print(xacc)
+print(yacc)
+# plot things up to visualize
+fig,ax = plt.subplots()
+ax.plot(xx,yy,'r*')
+circle1 = plt.Circle((5,5),5,fill=False)
+ax.add_patch(circle1)
+ax.plot(xacc,yacc)
+plt.show()
